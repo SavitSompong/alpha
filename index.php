873a28b86a8f9a2bb87c2f1ae7d036f849f85d85
Savit <34275778+SavitSompong@users.noreply.github.com>
2017-12-14 02:37:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+$channelSecret = ...; // Channel secret string
+$httpRequestBody = ...; // Request body string
+$hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
+$signature = base64_encode($hash);
+// Compare X-Line-Signature request header string and the signature
 
 echo date(DATE_RFC822)
 
